123a1a8c871428a04841545a8558c186b0ced69e
InfiniteAddie <61200130+InfiniteAddie@users.noreply.github.com>
2021-04-26 18:30:32 -0400

    updated main.go to display phonetic nums

--- main.go
+++ main.go
@@ -7,18 +7,50 @@ import "strconv"
 func main() {
 	/* Get list of numbers from cmd line */
 	args := os.Args[1:]
-	//fmt.Println(args)
 	
 	/* For each item in args, loop through each digit, and phonetically append to phoneticArgs list */
-	//var phoneticArgs = []string{}
+	var phoneticNums = []string{}
+	//iterate number-by-number
 	for i := 0; i < len(args); i ++ {
-		for j, c := range args[i] {
-			//fmt.Println(j, "=>", string(c))
+		var phoneticNum string = ""
+		//iterate digit-by-digit
+		for _, c := range args[i] {
 			num, err := strconv.Atoi(string(c))
 			if err != nil {
 				panic(err)
 			}
-			fmt.Println(j, "=>", num)
+			switch num {
+			case 0:
+				phoneticNum += "Zero"
+			case 1:
+				phoneticNum += "One"
+			case 2:
+				phoneticNum += "Two"
+			case 3:
+				phoneticNum += "Three"
+			case 4:
+				phoneticNum += "Four"
+			case 5:
+				phoneticNum += "Five"
+			case 6:
+				phoneticNum += "Six"
+			case 7:
+				phoneticNum += "Seven"
+			case 8:
+				phoneticNum += "Eight"
+			case 9:
+				phoneticNum += "Nine"
+			}
+		}
+		phoneticNums = append(phoneticNums, phoneticNum)
+	}
+
+	//output resulting list of phonetic numbers
+	for i := 0; i < len(phoneticNums); i ++ {
+		if i != len(phoneticNums) - 1 {
+			fmt.Print(phoneticNums[i], ", ")
+		} else {
+			fmt.Println(phoneticNums[i])
 		}
 	}
 }
